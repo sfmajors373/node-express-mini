73f1b2a078892c2894497070d62f16b27f80c9f4
sfmajors373 <sfmajors373@gmail.com>
2017-08-10 13:42:53 -0400

    Completed hangman

--- src/app.js
+++ src/app.js
@@ -14,6 +14,44 @@ const readWords = () => {
   return contents.split('\n');
 };
 
+const words = readWords();
+const index = Math.floor(Math.random() * words.length);
+const word = words[index];
+console.log(word);
+
 // TODO: your code to handle requests
+
+const guesses = {};
+
+server/post('/guess', (req, res) => {
+  const letter = req.body.letter;
+  // in json body letter must be in double quotes in order to be valid
+  console.log(letter);
+  if (!letter) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must provide a guess' });
+    return;
+  } else if (letter.length !== 1) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must provide a singular letter guess' });
+  } else if (guesses[letter]) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: `Cannot guess ${letter} again` });
+    return;
+  }
+  guesses.[letter] = true;
+});
+
+server.get('/', (req, res) => {
+  const wordSoFarArray = Array.fom(word).map((letter) => {
+    if(guesses[letter]) {
+      return letter;
+    }
+    return '-';
+  });
+  const wordSoFar = wordSoFarArray.join('');
+  res.send({ wordSoFar, guesses });
+});
+
 
 server.listen(3000);
